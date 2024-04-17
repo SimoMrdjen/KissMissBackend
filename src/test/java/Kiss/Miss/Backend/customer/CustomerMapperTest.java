fc59d7cb7f0784af83c6ad0460270f404ff6214b
Simo Mrdjen <dr.sizni@gmail.com>
2024-04-17 15:39:53 +0200

    add

---
+++ src/test/java/Kiss/Miss/Backend/customer/CustomerMapperTest.java
@@ -0,0 +1,26 @@
+package Kiss.Miss.Backend.customer;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerMapperTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void toEntity() {
+    }
+
+    @Test
+    void toDto() {
+    }
+}
